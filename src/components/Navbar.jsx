a77beff6c547630bf270121222613a35cdaac08c
Krish3282 <kvkrishna1938@gmail.com>
2025-04-10 10:05:41 +0530

    add mobile layout for nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const [isMenuOpen,setIsMenuOpen] = useState(false)
 
   return (
     <div className='fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm z-50 border-b border-gray-100 shadow-sm '>
-        <div className=' w-full container mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8  md:h-20 h-16'>
+        <div className=' container  w-full mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8  md:h-20 h-16'>
             
             {/* logo */}
             <div className='flex items-center gap-1 cursor-pointer sm:flex-1 md:flex-0'>
@@ -54,6 +54,27 @@ const [isMenuOpen,setIsMenuOpen] = useState(false)
             </button>
             
         </div>
+            {/* mobile menu for small screen */}
+            {
+              isMenuOpen && (
+                <div className='md:hidden bg-white border-t border-gray-100 py-4'>
+                  <div className='container mx-auto px-4 space-y-3'>
+                    {navLinks.map((link,index) =>(
+                      <a key={index} className= {`block text-sm font-medium py-2 ${activeLink === link.href ? 'text-blue-600' : 'text-gray-600 hover:text-gray-900'}`}
+                       onClick={()=>{setActiveLink(link.href)}}
+                      href={link.href}>{link.label}</a>
+                    ))}
+
+
+              <button>
+              <a href="#newsletter" className='block md:hidden bg-blue-600 text-white px-6 py-2.5 rounded-lg hover:bg-blue-700 text-sm font-medium
+               transition-all hover:shadow-lg hover:shadow-blue-100'>Get in touch</a>
+              </button>
+
+                  </div>
+                </div>
+              )
+            }
     </div>
   )
 }
