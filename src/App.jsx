743d2b00dfcde442fa2b4a5750785335a9d69ddd
Krish3282 <kvkrishna1938@gmail.com>
2025-04-18 18:25:29 +0530

    added few section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import'./App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import ComponentLogo from './components/ComponentLogo'
+import PurposeSection from './components/PurposeSection'
 
 const App = () => {
   return (
     <>
     <main className='relative min-h-screen overflow-x-hidden'>
-    <div className=' absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10'></div>
+    <div className=' absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-tr
+     from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10'></div>
     <div className='overflow-hidden'>
     <Navbar />
     <Hero />
+    <ComponentLogo />
+    <PurposeSection />
     </div>
+    
     </main>
     </>
   )
