dd6c5dcf5f94d4d98a7118bab69e6df82911044d
Krish3282 <kvkrishna1938@gmail.com>
2025-05-05 08:51:16 +0530

    added code ot newsletter section

--- src/components/NweslettterSection.jsx
+++ src/components/NweslettterSection.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { HiArrowRight } from 'react-icons/hi'
 
 const NweslettterSection = () => {
   return (
@@ -8,14 +9,33 @@ const NweslettterSection = () => {
             {/* gradient design */}
             <div className="absolute top-0 right-0 w-1/2 h-full bg-blue-700 clip-path-slant hidden md:block"></div>
 
-            <div>
+            <div className=' relative flex flex-col lg:flex-row items-center justify-between gap-8 lg:gap-12'>
                 {/* left content */}
-                <div> 
-                    <h2>Subscribe newsletter</h2>
+                <div className='text-white max-w-lg text-center md:text-left'> 
+                    <h2 className='text-2xl md:text-3xl lg:text-4xl font-medium mb-4'>Subscribe newsletter</h2>
+                    <p className='text-blue-100 text-sm sm:text-base'>Best Coders and best delivery at your service,best optimization.</p>
+                </div>
+                {/* rigth content */}
+                <div className='flex flex-col sm:flex-row gap-4 sm:gap-0'>
+                  <input type="text"
+                  className='w-full bg-white sm:w-auto md:w-80 px-4 sm:px-6 py-3 sm:py-4 rounded-xl sm:rounded-l-xl sm:rounded-r-none focus:outline-none '
+                   placeholder='Enter Your Email' />
+                   <button className=' flex w-full sm:w-auto cursor-pointer bg-green-500 text-white px-6 sm:px-8 py-3 sm:py-4 rounded-xl sm:rounded-l-none sm:rounded-r-lg items-center gap-2'>
+                      <span>
+                        <HiArrowRight className='size-5'/>
+                      </span>
+                   </button>
                 </div>
             </div>
         </div>
       </div>
+        <style>
+          {
+            `.clip-path-slant{
+            clip-path:polygon(20% 0, 100% 0,100% 100%,0 100%)
+            }`
+          }
+        </style>
     </section>
   )
 }
